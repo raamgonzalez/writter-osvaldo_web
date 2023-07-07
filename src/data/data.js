b7a8592cd86b0b97d1d0bcdcb848196d235379d9
Ramsés Gonzalez <gz.ramses.oc@gmail.com>
2023-07-07 16:26:49 -0300

    Add: new features

---
+++ src/data/data.js
@@ -0,0 +1,67 @@
+
+export const data = {
+	books : [
+		{
+			id: 1,
+			title: 'La ciudad del sol',
+			href: '#',
+			img: '/books/portada_ciudad-del-sol.jpg',
+			issue: 'Dramatico',
+			release: 2014,
+			description: 'lorem ipsum'
+		},
+		{
+			id: 2,
+			title: 'Neurosis',
+			href: '#',
+			img: '/books/portada_neurosis.png',
+			issue: 'Dramatico',
+			release: 2014,
+			description: 'lorem ipsum'
+		},
+		{
+			id: 3,
+			title: 'Tierra sin mal',
+			href: '#',
+			img: '/books/portada_tierra-sin-mal.jpg',
+			issue: 'Dramatico',
+			release: 2014,
+			description: 'lorem ipsum'
+		},
+		{
+			id: 4,
+			title: 'Razones seminales',
+			href: '#',
+			img: '/books/portada_razones-seminales.jpg',
+			issue: 'Dramatico',
+			release: 2014,
+			description: 'lorem ipsum'
+		},
+		{
+			id: 5,
+			title: 'Destellos bajo el agua',
+			href: '#',
+			img: '/books/portada_destellos-bajo-el-agua.jpg',
+			issue: 'Dramatico',
+			release: 2014,
+			description: 'lorem ipsum'
+		}
+	],
+	phrases : [
+		{
+			id: 1,
+			phrase: 'Dios ha muerto',
+			author: 'Friedrich Nietzsche'
+		},
+		{
+			id: 2,
+			phrase: 'Yo soy yo y mis circunstancias',
+			author: 'José Ortega y Gasset'
+		},
+		{
+			id: 3,
+			phrase: 'Es justicia y no caridad lo que el mundo necesita',
+			author: 'Mary Wollstonecraft'
+		},
+	]
+}
