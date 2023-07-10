d1493416ec9f6ff11ec16ef20514a7ad48979dc4
Ramsés Gonzalez <gz.ramses.oc@gmail.com>
2023-07-10 01:38:37 -0300

    Add: features, Filmoteca, Contacto

--- src/data/data.js
+++ src/data/data.js
@@ -65,20 +65,87 @@ export const data = {
 		},
 	],
 	notes: [
-	{
-		id: 1,
-		title: 'Entrevista - Editorial Adarve',
-		content: 'Presentación Neurosis',
-		href: '#',
-	},{
-		id: 1,
-		title: 'Entrevista - Editorial Adarve',
-		content: 'Presentación Neurosis',
-		href: '#',
-	},{
-		id: 1,
-		title: 'Entrevista - Editorial Adarve',
-		content: 'Presentación Neurosis',
-		href: '#',
-	}]
+		{
+			id: 1,
+			title: 'Entrevista - Editorial Adarve',
+			content: 'Presentación Neurosis',
+			href: 'https://developer.mozilla.org/es/docs/Web/HTML/Element/button',
+			important: true
+		},{
+			id: 2,
+			title: 'Entrevista - Editorial Adarve',
+			content: 'Presentación Neurosis',
+			href: '#',
+			important: true
+		},{
+			id: 3,
+			title: 'Entrevista - Editorial Adarve',
+			content: 'Presentación Neurosis',
+			href: '#',
+			important: true
+		}
+		,{
+			id: 4,
+			title: 'Entrevista - Editorial Adarve',
+			content: 'Presentación Neurosis',
+			href: '#',
+			important: false
+		}
+		,{
+			id: 5,
+			title: 'Entrevista - Editorial Adarve',
+			content: 'Presentación Neurosis',
+			href: '#',
+			important: false
+		}
+	],
+	films: [
+		{
+			id: 1,
+			title: 'La ciudad del sol',
+			poster: '/films/poster_ciudad-del-sol.jpg',
+			href: 'https://www.youtube.com/watch?v=_SSMIqFoiS4',
+			movie: '#'
+		},
+		{
+			id: 2,
+			title: 'Neurosis',
+			poster: '/films/poster_neurosis.jpg',
+			href: '#',
+			movie: '#'
+		},
+		{
+			id: 3,
+			title: 'Tierra sin mal',
+			poster: '/films/poster_tierra-sin-mal.jpg',
+			href: '#',
+			movie: '#'
+		},
+	],
+	contact:[
+		{
+			id: 1,
+			title: 'Diario digital',
+			href: 'https://www.debateyconvergencia.com.ar',
+			state: true
+		},
+		{
+			id: 2,
+			title: 'Facebook',
+			href: 'https://www.facebook.com/Adarve-Ediciones-106385984031',
+			state: true
+		},
+		{
+			id: 3,
+			title: 'Twitter',
+			href: 'https://twitter.com/AdarveEdiciones',
+			state: true
+		},
+		{
+			id: 4,
+			title: 'Instagram',
+			href: 'https://www.instagram.com/adarveediciones/',
+			state: true
+		},
+	]
 }
