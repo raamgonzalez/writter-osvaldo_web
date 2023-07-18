97875e5c292cb6f17204685d59f07dcc88674347
Ramsés Gonzalez <gz.ramses.oc@gmail.com>
2023-07-18 15:19:20 -0300

    Edit: books in page, add buy button and styles

--- src/data/data.js
+++ src/data/data.js
@@ -6,61 +6,85 @@ export const data = {
 			title: 'La ciudad del sol',
 			href: '#',
 			img: '/books/portada_ciudad-del-sol.jpg',
-			issue: 'Dramatico',
-			release: 2014,
+			issue: 'Novela negra',
+			release: 2017,
 			description: 'Un adolescente escapa de una ciudad dominada por una junta militar opresiva en busca de solidez. Cruzando ilegalmente la frontera mexicana, llega a Manhattan y se sumerge en un estilo de vida marcado por la política, el rock, las drogas y la muerte. La historia explora la confrontación con mundos paralelos y nuevos códigos de conducta, mientras transcurre en Buenos Aires, Monterrey y la vibrante ciudad de Manhattan. El autor revela la oscuridad y la atmósfera subterránea de Manhattan, creando una narrativa en tiempos difusos y paralelos.',
-			link01: 'https://www.libreriasinopsis.com/libro/la-ciudad-del-sol_293220',
-			link02: 'https://www.ellibrotecnico.com/libro/la-ciudad-del-sol_507799',
-			link03: '#'
+			links: [
+				{
+					id: 1,
+					href:'https://www.libreriasinopsis.com/libro/la-ciudad-del-sol_293220',
+					store: 'Libreria Sinopsis',
+				},
+				{
+					id: 2,
+					href:'https://www.ellibrotecnico.com/libro/la-ciudad-del-sol_507799',
+					store: 'Libreria El libro',
+				}
+			]
 		},
 		{
 			id: 2,
 			title: 'Neurosis',
 			href: '#',
 			img: '/books/portada_neurosis.png',
-			issue: 'Dramatico',
-			release: 2014,
+			issue: 'Ficción Contemporánea',
+			release: 2018,
 			description: 'Una exploración de la neurosis como una estrategia para evitar emociones y pensamientos amenazantes. Ambientada en un país devastado por lluvias que borran los límites de la civilización y deshacen los valores sociales, el poder económico y político busca soluciones sin preocuparse por las consecuencias. Mientras tanto, periodistas investigan las causas de las inundaciones y el comportamiento del poder político. La lluvia transforma el paisaje humano y da lugar a la aparición de "lagartos". La novela envuelve al lector en el barro del paisaje y la condición humana, con un mensaje final de que el amor puede salvar al mundo.',
-			link01: 'https://www.amazon.com/-/es/%C3%93svaldo-Gonz%C3%A1lez-Iglesias/dp/8417362487',
-			link02: '#',
-			link03: '#'
+			links: [
+							{
+								id: 1,
+								href:'https://www.amazon.com/-/es/%C3%93svaldo-Gonz%C3%A1lez-Iglesias/dp/8417362487',
+								store: 'Amazon',
+							}
+						]
 		},
 		{
 			id: 3,
 			title: 'Tierra sin mal',
 			href: '#',
 			img: '/books/portada_tierra-sin-mal.jpg',
-			issue: 'Dramatico',
+			issue: 'Dramático',
 			release: 2014,
 			description: 'Un aristócrata contrata a un investigador privado para encontrar a su hija perdida en la selva tras descubrir que tuvo una relación con una mujer guaraní. A medida que el investigador se adentra en los pueblos en busca de respuestas, se desencadenan conflictos que ponen en peligro su seguridad y la de la joven desaparecida. La trama revela historias de trata de personas y un entramado de poder en el que intervienen personajes de diferentes estratos sociales. La cultura guaraní, arraigada a la tierra y luchando por recuperar su historia, desafía la comprensión racional. Mientras tanto, en el conurbano bonaerense, mujeres jóvenes aparecen muertas, muchas provenientes de la provincia de Villanueva y vinculadas a la prostitución. Un amuleto misterioso que llevan en sus cuellos se convierte en el enigma central de la trama. Los conflictos legales y gubernamentales, el lavado de activos y el colapso económico llevan al país al borde del estallido social.',
-			link01: '#',
-			link02: '#',
-			link03: '#'
-			
+			links: [
+							{
+								id: 1,
+								href:'',
+								store: '',
+							}
+						]	
 		},
 		{
 			id: 4,
 			title: 'Razones seminales',
 			href: '#',
 			img: '/books/portada_razones-seminales.jpg',
-			issue: 'Dramatico',
-			release: 2014,
+			issue: 'Policial Negro',
+			release: 2022,
 			description: 'Hechos que enlazan tres ciudades. Conjura, traición, desengaño. Dos venezolanas aparecen muertas de forma atroz en los muelles de Long Island, la venganza de los latinos está en marcha. Greis, una periodista el The New Day, descubre parte del complot pero sin percibir que todo es un engaño, una pantalla. El fin es solidificar el sistema estadounidense para regular sus libertades y de esa forma terminar con los movimientos que, como el Black Lives Matter, viene azotando a la ciudad produciendo múltiples desmanes. La cultura de la cancelación comienza a tomar cuerpo en las instituciones, en los medios de comunicación, en el vocabulario cotidiano, produciendo un clima de autoritarismo y discriminación, quizás peor del que se quiere evitar.',
-			link01: 'https://autoresdeargentina.mercadoshops.com.ar/MLA-1329094593-razones-seminales-osvaldo-gonzalez-iglesias-_JM#position=1&search_layout=stack&type=item&tracking_id=155c4515-aff5-495a-9f68-33cd53966b8b',
-			link02: '#',
-			link03: '#'
+			links: [
+				{
+					id: 1,
+					href:'https://autoresdeargentina.mercadoshops.com.ar/MLA-1329094593-razones-seminales-osvaldo-gonzalez-iglesias-_JM#position=1&search_layout=stack&type=item&tracking_id=155c4515-aff5-495a-9f68-33cd53966b8b',
+					store: 'Mercado Libre',
+				}
+			]	
 		},
 		{
 			id: 5,
 			title: 'Destellos bajo el agua',
 			href: '#',
 			img: '/books/portada_destellos-bajo-el-agua.jpg',
-			issue: 'Dramatico',
-			release: 2014,
+			issue: 'Novela negra',
+			release: 2022,
 			description: 'En esta novela, un joven cruza la frontera mexicana en busca de estabilidad. En Manhattan, se sumerge en una vida caótica junto a sus amigos, influenciados por la música. La historia se desarrolla en Buenos Aires, Monterrey y Manhattan, explorando temas como la política, la droga y la búsqueda de sentido. Los personajes se debaten entre la razón y la demencia, enfrentando el fracaso, el amor y la traición. Con un suspenso cautivador, la trama revela destinos sorprendentes. El autor profundiza en los individuos, desentrañando la esencia del Ser y sus luchas internas entre la razón y la demencia. El sentimiento de fracaso, la búsqueda de sustancia, el amor, la amistad y la traición son temas explorados.',
-			link01: 'https://www.amazon.com/-/es/Osvaldo-Gonz%C3%A1lez-Iglesias-ebook/dp/B0BF619J67',
-			link02: '#',
-			link03: '#'
+			links: [
+							{
+								id: 1,
+								href:'https://www.amazon.com/-/es/Osvaldo-Gonz%C3%A1lez-Iglesias-ebook/dp/B0BF619J67',
+								store: 'Amazon',
+							}
+						]	
 		}
 	],
 	phrases : [
