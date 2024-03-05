7cf1dfa024d28053bc09f76d6c4fd6a2fb1d4f40
Ramsés Gonzalez <gz.ramses.oc@gmail.com>
2024-03-05 01:45:23 -0300

    Add: Refactor in pages

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,6 +4,22 @@ import animations from '@midudev/tailwind-animations'
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
+		screens: {
+			'sm': '460px',
+			// => @media (min-width: 640px) { ... }
+
+			'md': '768px',
+			// => @media (min-width: 768px) { ... }
+
+			'lg': '1024px',
+			// => @media (min-width: 1024px) { ... }
+
+			'xl': '1280px',
+			// => @media (min-width: 1280px) { ... }
+
+			'2xl': '1536px',
+			// => @media (min-width: 1536px) { ... }
+		},
 		margin: {
 			xxl: '6rem',
 			xxl: '5rem',
